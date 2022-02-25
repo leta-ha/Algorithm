54491ce8f4e247f86127b44471f9122a9cf0abc3
leta-ha <letaha@naver.com>
2022-02-25 21:23:37 +0900

    Create boj-15655.py

---
+++ boj-15655.py
@@ -0,0 +1,19 @@
+import sys
+input = sys.stdin.readline
+
+def dfs(s): #s: start
+    if len(ans) == m:
+        print(*ans)
+        return
+    
+    for i in num:
+        if i > s and i not in ans:
+            ans.append(i)
+            dfs(i)
+            ans.pop()
+
+n, m = map(int, input().split())
+num = list(map(int, input().split()))
+num.sort()
+ans = []
+dfs(0)
